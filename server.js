adbd2d9040b7dd89a19273942f527e64a9240505
Joshua Mora <josue_mx_2012@live.com>
2020-10-28 23:32:36 -0500

    ready to publish

--- server.js
+++ server.js
@@ -1,40 +1,47 @@
 require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const MOVIES = require('./movies-data-small.json')
-
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
-
 app.use(function validateBearerToken(req, res, next) {
+
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
+
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
+
   next()
 })
 
+//handle the get request
 app.get('/movie', function handleGetMovie(req, res) {
   let response = MOVIES;
 
+  //filter by genre
   if (req.query.genre) {
     response = response.filter(movie =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     )
   }
 
+  //filter by country
   if (req.query.country) {
     response = response.filter(movie =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
     )
   }
 
+  //filter by average votes
   if (req.query.avg_vote) {
     response = response.filter(movie =>
       Number(movie.avg_vote) >= Number(req.query.avg_vote)
@@ -42,10 +49,22 @@ app.get('/movie', function handleGetMovie(req, res) {
   }
 
   res.json(response)
+
 })
 
-const PORT = 9000
+p.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+
 })
